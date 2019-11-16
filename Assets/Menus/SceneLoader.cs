085167b7c454e6611568bd71c4c6b5e54f284e8d
Christian D Alvarado <32400508+calva038@users.noreply.github.com>
2019-11-16 14:49:25 -0800

    Start Menu

---
+++ Assets/Menus/SceneLoader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void startButton()
+    {
+            SceneManager.LoadScene(1);
+    }
+}
